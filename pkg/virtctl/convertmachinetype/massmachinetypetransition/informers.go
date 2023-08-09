981814df33d62f1dd4c26945a1efa508717c833b
prnaraya <prnaraya@redhat.com>
2023-08-08 20:01:58 -0500

    Addressing Itamar's feedback re:Job image source code
    Added controller struct to reduce API calls
    Other small adjustments in line with the feedback.
    
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- pkg/virtctl/convertmachinetype/massmachinetypetransition/informers.go
+++ pkg/virtctl/convertmachinetype/massmachinetypetransition/informers.go
@@ -3,15 +3,40 @@ package massmachinetypetransition
 import (
 	"context"
 	"fmt"
-	"time"
 
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/cache"
 	k6tv1 "kubevirt.io/api/core/v1"
 	"kubevirt.io/client-go/kubecli"
 )
+
+type JobController struct {
+	VmiInformer cache.SharedIndexInformer
+	VirtClient  kubecli.KubevirtClient
+	ExitJob     chan struct{}
+}
+
+func NewJobController(
+	vmiInformer cache.SharedIndexInformer,
+	virtClient kubecli.KubevirtClient,
+	exitJob chan struct{},
+) (*JobController, error) {
+	c := &JobController{
+		VmiInformer: vmiInformer,
+		VirtClient:  virtClient,
+		ExitJob:     exitJob,
+	}
+
+	_, err := vmiInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		DeleteFunc: c.HandleDeletedVmi,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
 
 func getVirtCli() (kubecli.KubevirtClient, error) {
 	clientConfig, err := kubecli.GetKubevirtClientConfig()
@@ -27,30 +52,13 @@ func getVirtCli() (kubecli.KubevirtClient, error) {
 	return virtCli, err
 }
 
-func getVmiInformer(virtCli kubecli.KubevirtClient) (cache.SharedIndexInformer, error) {
-	listWatcher := cache.NewListWatchFromClient(virtCli.RestClient(), "virtualmachineinstances", namespace, fields.Everything())
-	vmiInformer := cache.NewSharedIndexInformer(listWatcher, &k6tv1.VirtualMachineInstance{}, 1*time.Hour, cache.Indexers{})
-
-	vmiInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		DeleteFunc: HandleDeletedVmi,
-	})
-
-	return vmiInformer, nil
-}
-
-func HandleDeletedVmi(obj interface{}) {
+func (c *JobController) HandleDeletedVmi(obj interface{}) {
 	vmi, ok := obj.(*k6tv1.VirtualMachineInstance)
 	if !ok {
 		return
 	}
 
-	virtCli, err := getVirtCli()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	vm, err := virtCli.VirtualMachine(vmi.Namespace).Get(context.Background(), vmi.Name, &k8sv1.GetOptions{})
+	vm, err := c.VirtClient.VirtualMachine(vmi.Namespace).Get(context.Background(), vmi.Name, &k8sv1.GetOptions{})
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -61,75 +69,44 @@ func HandleDeletedVmi(obj interface{}) {
 		return
 	}
 
-	// check that VM machine type is not outdated in the case that a VM has been restarted for
-	// a different reason before updating its machine type
-	pendingUpdate, _ := VerifyMachineType(vm.Spec.Template.Spec.Domain.Machine.Type)
+	// check that VM machine type is not outdated in the case that a VM
+	// has been restarted for a different reason before its machine type
+	// has been updated
+	pendingUpdate, _ := IsMachineTypeUpdated(vmi.Status.Machine.Type)
 	if pendingUpdate {
 		return
 	}
 
-	// get VMI name in the format namespace/name
-	vmiKey, err := cache.MetaNamespaceKeyFunc(vmi)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// check if deleted VMI is in list of VMIs that need to be restarted
-	_, exists := VmisPendingUpdate[vmiKey]
-	if !exists {
-		return
-	}
-
 	// remove warning label from VM
-	// if removing the warning label fails, exit before removing VMI from list
-	// since the label is still there to tell the user to restart, it wouldn't
-	// make sense to have a mismatch between the number of VMs with the label
-	// and the number of VMIs in the list of VMIs pending update.
-
-	err = RemoveWarningLabel(virtCli, vm)
+	err = c.RemoveWarningLabel(vm)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// remove deleted VMI from list
-	delete(VmisPendingUpdate, vmiKey)
-
-	// check if VMI list is now empty, to signal exiting the job
-	if len(VmisPendingUpdate) == 0 {
-		// as a sanity check, ensure all 'restart-vm-required' labels
-		// on affected VMs are cleared using label selector
-		vmList, err := virtCli.VirtualMachine(namespace).List(context.Background(), &k8sv1.ListOptions{
-			LabelSelector: "restart-vm-required=true",
-		})
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if len(vmList.Items) != 0 {
-			// if there are VMs that still have the label, add them back to
-			// the VmisPendingUpdate list
-			for _, vm := range vmList.Items {
-				vmKey, err := cache.MetaNamespaceKeyFunc(vm)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				VmisPendingUpdate[vmKey] = struct{}{}
-			}
-			return
-		}
-
-		close(exitJob)
+	// ensure all 'restart-vm-required' labels on affected VMs
+	// are cleared using label selector
+	numVmisPendingUpdate := c.numVmisPendingUpdate()
+	if numVmisPendingUpdate != 0 {
+		return
 	}
+
+	close(c.ExitJob)
 }
 
-func RemoveWarningLabel(virtCli kubecli.KubevirtClient, vm *k6tv1.VirtualMachine) error {
+func (c *JobController) RemoveWarningLabel(vm *k6tv1.VirtualMachine) error {
 	removeLabel := `[{"op": "remove", "path": "/metadata/labels/restart-vm-required"}]`
-	_, err := virtCli.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, []byte(removeLabel), &k8sv1.PatchOptions{})
+	_, err := c.VirtClient.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.JSONPatchType, []byte(removeLabel), &k8sv1.PatchOptions{})
 	return err
 }
 
-func GetExitJobChannel() chan struct{} {
-	return exitJob
+func (c *JobController) numVmisPendingUpdate() int {
+	vmList, err := c.VirtClient.VirtualMachine(namespace).List(context.Background(), &k8sv1.ListOptions{
+		LabelSelector: "restart-vm-required=true",
+	})
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	return len(vmList.Items)
 }
