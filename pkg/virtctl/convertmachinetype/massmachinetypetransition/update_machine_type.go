5cedc667e49333e62476e38389b115687cc5a729
prnaraya <prnaraya@redhat.com>
2023-08-08 20:01:58 -0500

    Addressing Itamar's feedback re:Job image source code
    Added controller struct to reduce API calls
    Other small adjustments in line with the feedback.
    
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- pkg/virtctl/convertmachinetype/massmachinetypetransition/update_machine_type.go
+++ pkg/virtctl/convertmachinetype/massmachinetypetransition/update_machine_type.go
@@ -7,9 +7,7 @@ import (
 
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/tools/cache"
 	k6tv1 "kubevirt.io/api/core/v1"
-	"kubevirt.io/client-go/kubecli"
 )
 
 // using these as consts allows us to easily modify the program to update as newer versions are released
@@ -20,9 +18,6 @@ const (
 )
 
 var (
-	VmisPendingUpdate = make(map[string]struct{})
-	exitJob           = make(chan struct{})
-
 	// by default, update machine type across all namespaces
 	namespace     = k8sv1.NamespaceAll
 	labelSelector = ""
@@ -31,33 +26,38 @@ var (
 	restartNow = false
 )
 
-func VerifyMachineType(machineType string) (bool, string) {
+func IsMachineTypeUpdated(machineType string) (isUpdated bool, updatedMachineType string, err error) {
+
 	// if the machine type is "q35", we want the VM to behave as though its machine type has been updated
 	if machineType == "q35" {
-		return true, machineType
+		return true, machineType, nil
 	}
 
 	machineTypeSubstrings := strings.Split(machineType, "-")
+
+	if len(machineTypeSubstrings) != 3 {
+		return false, machineType, fmt.Errorf("invalid machine type format")
+	}
 
 	if len(machineTypeSubstrings) == 3 {
 		version := machineTypeSubstrings[2]
 		if strings.Contains(version, "rhel") && version < MinimumSupportedMachineTypeVersion {
 			machineTypeSubstrings[2] = LatestMachineTypeVersion
 			machineType = strings.Join(machineTypeSubstrings, "-")
-			return true, machineType
+			return true, machineType, nil
 		}
 	}
-	return false, machineType
+	return false, machineType, nil
 }
 
-func UpdateMachineTypes(virtCli kubecli.KubevirtClient) error {
+func (c *JobController) UpdateMachineTypes() error {
 	listOpts := &k8sv1.ListOptions{}
 	if labelSelector != "" {
 		listOpts = &k8sv1.ListOptions{
 			LabelSelector: labelSelector,
 		}
 	}
-	vmList, err := virtCli.VirtualMachine(namespace).List(context.Background(), listOpts)
+	vmList, err := c.VirtClient.VirtualMachine(namespace).List(context.Background(), listOpts)
 
 	if err != nil {
 		return err
@@ -65,7 +65,12 @@ func UpdateMachineTypes(virtCli kubecli.KubevirtClient) error {
 
 	for _, vm := range vmList.Items {
 		machineType := vm.Spec.Template.Spec.Domain.Machine.Type
-		needsUpdate, machineType := VerifyMachineType(machineType)
+		needsUpdate, machineType, err := IsMachineTypeUpdated(machineType)
+
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
 
 		// skip VM if its machine type is supported
 		if !needsUpdate {
@@ -75,7 +80,7 @@ func UpdateMachineTypes(virtCli kubecli.KubevirtClient) error {
 		if machineType != "q35" {
 			updateMachineType := fmt.Sprintf(`{"spec":{"template":{"spec":{"domain":{"machine":{"type":"%s"}}}}}}`, machineType)
 
-			_, err := virtCli.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.MergePatchType, []byte(updateMachineType), &k8sv1.PatchOptions{})
+			_, err := c.VirtClient.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.MergePatchType, []byte(updateMachineType), &k8sv1.PatchOptions{})
 			if err != nil {
 				fmt.Print(err)
 				continue
@@ -84,29 +89,40 @@ func UpdateMachineTypes(virtCli kubecli.KubevirtClient) error {
 
 		// add label to running VMs that a restart is required for change to take place
 		if *vm.Spec.Running {
-			vmi, err := virtCli.VirtualMachineInstance(vm.Namespace).Get(context.Background(), vm.Name, &k8sv1.GetOptions{})
+			vmi, err := c.VirtClient.VirtualMachineInstance(vm.Namespace).Get(context.Background(), vm.Name, &k8sv1.GetOptions{})
 			if err != nil {
 				fmt.Print(err)
 				continue
 			}
+
+			// verify that VMI is in running phase
+			// if vmi.Status.Phase != k6tv1.Running {
+
+			// }
 
 			// only need to restart VM if VMI machine type is outdated (in the case of "q35" machine types
-			needsUpdate, _ := VerifyMachineType(vmi.Status.Machine.Type)
+			needsUpdate, _, err := IsMachineTypeUpdated(vmi.Status.Machine.Type)
+
+			// needsUpdate will always be false when the function returns and error
 			if !needsUpdate {
+				if err != nil {
+					fmt.Print(err)
+				}
 				continue
 			}
 
-			// adding the warning label to the VMs regardless if we restart them now or if the user does it manually
-			// shouldn't matter, since the deletion of the VMI will remove the label and remove the vmi list anyway
-			err = AddWarningLabel(virtCli, &vm)
-			if err != nil {
-				fmt.Print(err)
-				continue
+			// if force restart flag is set, restart running VMs immediately
+			if restartNow {
+				err = c.VirtClient.VirtualMachine(vm.Namespace).Restart(context.Background(), vm.Name, &k6tv1.RestartOptions{})
+				if err != nil {
+					fmt.Print(err)
+					continue
+				}
 			}
-		}
 
-		if restartNow {
-			err = virtCli.VirtualMachine(vm.Namespace).Restart(context.Background(), vm.Name, &k6tv1.RestartOptions{})
+			// adding the warning label to the VMs to indicate to the user
+			// they must manually be restarted
+			err = c.AddWarningLabel(&vm)
 			if err != nil {
 				fmt.Print(err)
 				continue
@@ -116,22 +132,11 @@ func UpdateMachineTypes(virtCli kubecli.KubevirtClient) error {
 	return nil
 }
 
-func AddWarningLabel(virtCli kubecli.KubevirtClient, vm *k6tv1.VirtualMachine) error {
+func (c *JobController) AddWarningLabel(vm *k6tv1.VirtualMachine) error {
 	addLabel := `{"metadata":{"labels":{"restart-vm-required":"true"}}}`
 
-	_, err := virtCli.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.MergePatchType, []byte(addLabel), &k8sv1.PatchOptions{})
-	if err != nil {
-		return err
-	}
-
-	// get VM name in the format namespace/name
-	vmKey, err := cache.MetaNamespaceKeyFunc(vm)
-	if err != nil {
-		return err
-	}
-	VmisPendingUpdate[vmKey] = struct{}{}
-
-	return nil
+	vm, err := c.VirtClient.VirtualMachine(vm.Namespace).Patch(context.Background(), vm.Name, types.MergePatchType, []byte(addLabel), &k8sv1.PatchOptions{})
+	return err
 }
 
 func SetTestRestartNow(testRestart bool) {
