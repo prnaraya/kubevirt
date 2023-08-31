b735e14d85be36b884cbc8ae8d6db6f4166ca4c9
prnaraya <prnaraya@redhat.com>
2023-08-30 22:51:08 -0500

    Pass grace-period to virt-controller when stopping or restarting VM
    The grace-period was never getting passed to virt-controller which
    deletes the VMI, causing Delete() to use the default TerminationGracePeriodSeconds
    in the VMI Spec. In some cases, when force stopping a VM, this TerminationGracePeriodSeconds
    was not getting updated with the new grace period. This is particularly a
    problem with Windows VMIs, which by default have a TerminationGracePeriodSeconds
    of 3600 seconds (one hour). If the user wants to force stop/restart a VM with
    grace-period=0, we should honor this and delete the VMI with that grace-period.
    
    Signed-off-by: prnaraya <prnaraya@redhat.com>

--- pkg/virt-api/rest/subresource.go
+++ pkg/virt-api/rest/subresource.go
@@ -278,19 +278,24 @@ func getRunningJson(vm *v1.VirtualMachine, running bool) string {
 		runStrategy = v1.RunStrategyAlways
 	}
 	if vm.Spec.RunStrategy != nil {
-		return fmt.Sprintf("{\"spec\":{\"runStrategy\": \"%s\"}}", runStrategy)
+		return fmt.Sprintf(`{"spec":{"runStrategy": "%s"}}`, runStrategy)
 	} else {
-		return fmt.Sprintf("{\"spec\":{\"running\": %t}}", running)
+		return fmt.Sprintf(`{"spec":{"running": %t}}`, running)
 	}
 }
 
 func getUpdateTerminatingSecondsGracePeriod(gracePeriod int64) string {
-	return fmt.Sprintf("{\"spec\":{\"terminationGracePeriodSeconds\": %d }}", gracePeriod)
+	return fmt.Sprintf(`{"spec":{"terminationGracePeriodSeconds": %d }}`, gracePeriod)
 }
 
 func (app *SubresourceAPIApp) patchVMStatusStopped(vmi *v1.VirtualMachineInstance, vm *v1.VirtualMachine, response *restful.Response, bodyStruct *v1.StopOptions) (error, error) {
+	stopChangeRequestData := make(map[string](string))
+	if bodyStruct.GracePeriod != nil {
+		stopChangeRequestData["grace-period"] = fmt.Sprintf("%d", *bodyStruct.GracePeriod)
+	}
+
 	bodyString, err := getChangeRequestJson(vm,
-		v1.VirtualMachineStateChangeRequest{Action: v1.StopRequest, UID: &vmi.UID})
+		v1.VirtualMachineStateChangeRequest{Action: v1.StopRequest, Data: stopChangeRequestData, UID: &vmi.UID})
 	if err != nil {
 		writeError(errors.NewInternalError(err), response)
 		return nil, err
@@ -411,8 +416,13 @@ func (app *SubresourceAPIApp) RestartVMRequestHandler(request *restful.Request, 
 		return
 	}
 
+	stopChangeRequestData := make(map[string](string))
+	if bodyStruct.GracePeriodSeconds != nil {
+		stopChangeRequestData["grace-period"] = fmt.Sprintf("%d", *bodyStruct.GracePeriodSeconds)
+	}
+
 	bodyString, err := getChangeRequestJson(vm,
-		v1.VirtualMachineStateChangeRequest{Action: v1.StopRequest, UID: &vmi.UID},
+		v1.VirtualMachineStateChangeRequest{Action: v1.StopRequest, Data: stopChangeRequestData, UID: &vmi.UID},
 		v1.VirtualMachineStateChangeRequest{Action: v1.StartRequest})
 	if err != nil {
 		writeError(errors.NewInternalError(err), response)
@@ -650,6 +660,7 @@ func (app *SubresourceAPIApp) StopVMRequestHandler(request *restful.Request, res
 	var oldGracePeriodSeconds int64
 	patchType := types.MergePatchType
 	var patchErr error
+
 	if hasVMI && !vmi.IsFinal() && bodyStruct.GracePeriod != nil {
 		// used for stopping a VM with RunStrategyHalted
 		if vmi.Spec.TerminationGracePeriodSeconds != nil {
@@ -672,7 +683,7 @@ func (app *SubresourceAPIApp) StopVMRequestHandler(request *restful.Request, res
 			return
 		}
 		if bodyStruct.GracePeriod == nil || (vmi.Spec.TerminationGracePeriodSeconds != nil && *bodyStruct.GracePeriod >= oldGracePeriodSeconds) {
-			writeError(errors.NewConflict(v1.Resource("virtualmachine"), name, fmt.Errorf("%v only supports manual stop requests with a shorter graceperiod", v1.RunStrategyHalted)), response)
+			writeError(errors.NewConflict(v1.Resource("virtualmachine"), name, fmt.Errorf("%v only supports manual stop requests with a shorter grace-period", v1.RunStrategyHalted)), response)
 			return
 		}
 		// same behavior as RunStrategyManual
